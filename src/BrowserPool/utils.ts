695464bbab2a16502f6e54f59eb51a3f6417cc80
luin <i@zihua.li>
2018-11-25 21:05:41 +0800

    perf: set executablePath explicitly for better perf

--- src/BrowserPool/utils.ts
+++ src/BrowserPool/utils.ts
@@ -1,15 +1,22 @@
-import {Browser, launch} from 'puppeteer'
+import * as puppeteer from 'puppeteer'
 import Debug from '../utils/debug'
+import * as packageJson from 'puppeteer/package.json'
+
+const browserFetcher = (puppeteer as any).createBrowserFetcher()
+const revision = packageJson.puppeteer.chromium_revision
+const {executablePath} = browserFetcher.revisionInfo(revision)
 
 const debug = Debug('browser-pool:utils')
+debug('got executablePath: %s', executablePath)
 
-export async function launchBrowser (retries: number = 1): Promise<Browser> {
+export async function launchBrowser (retries: number = 1): Promise<puppeteer.Browser> {
   const launchArgs = {
+    executablePath,
     args: ['--disable-dev-shm-usage', '--no-sandbox', '--disable-setuid-sandbox']
   }
 
   try {
-    return await launch(launchArgs)
+    return await puppeteer.launch(launchArgs)
   } catch (err) {
     console.error(err)
 
